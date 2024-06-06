30484bbb7ab40746b11dc8d2783118e7c01a973a
FedericoBohl <federicobohl@uca.edu.ar>
2024-06-06 10:53:27 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,12 @@ def make_acciones(data_now : pd.DataFrame):
     data=pd.read_csv('data_bolsa/bolsa_arg.csv',delimiter=';')
     data_merv=data[data['Merv']==True]
     data_merv=pd.merge(data_now,data_merv,on='simbolo').dropna()
-    data_merv['variacionPorcentual']=data_merv["variacionPorcentual"]#*100
+    #data_merv['variacionPorcentual']=data_merv["variacionPorcentual"]*100
     data_gen=data[data['Merv']==False]
     data_gen=pd.merge(data_now,data_gen,on='simbolo').dropna()
     st.dataframe(data_merv)
     #-------------- Fig del Merval  --------------
-    df_grouped = data_merv.groupby(["Sector","Nombre Completo"])[["CAP (MM)","variacionPorcentual","Nombre Completo","ultimoPrecio"]].min().reset_index()
+    df_grouped = data_merv.groupby(["Sector","Nombre Completo"])#[["CAP (MM)","variacionPorcentual","Nombre Completo","ultimoPrecio"]].min().reset_index()
     st.dataframe(df_grouped)
     fig_merv = px.treemap(df_grouped, 
                     path=[px.Constant("Bolsa Argentina"), 'Sector',  'Nombre Completo'], #Quite 'Industria', en 3
