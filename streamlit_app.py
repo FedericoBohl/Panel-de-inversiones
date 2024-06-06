d52d2a31f1dff6c1c3942ecd1b6bf92fcd2caf6d
FedericoBohl <federicobohl@uca.edu.ar>
2024-06-06 10:57:59 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,12 @@ def make_acciones(data_now : pd.DataFrame):
     data_gen=pd.merge(data_now,data_gen,on='simbolo').dropna()
     st.dataframe(data_merv)
     #-------------- Fig del Merval  --------------
-    df_grouped = data_merv.groupby(["Sector","Nombre Completo"])[["CAP (MM)","variacionPorcentual","Nombre Completo","ultimoPrecio"]].min().reset_index()
+    df_grouped = data_merv.groupby(["Sector", "Nombre Completo"]).agg({
+        "CAP (MM)": "min",
+        "variacionPorcentual": "min",  # O la agregación que tenga sentido
+        "Nombre Completo": "first",    # Como ya es parte del índice, esto es solo por seguridad
+        "ultimoPrecio": "min"          # O la agregación que tenga sentido
+    }).reset_index()
     st.dataframe(df_grouped)
     fig_merv = px.treemap(df_grouped, 
                     path=[px.Constant("Bolsa Argentina"), 'Sector',  'Nombre Completo'], #Quite 'Industria', en 3
