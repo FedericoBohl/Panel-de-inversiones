d5fc826d7373826f561ced46e3b114a26dff1bda
FedericoBohl <federicobohl@uca.edu.ar>
2024-06-22 18:25:38 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,10 +206,10 @@ if 'iol' in S:
                 fig.update_layout(margin=dict(l=1, r=1, t=1, b=1))
                 c2.plotly_chart(fig,use_container_width=True)
                 c21.subheader(':green[Top Winners]')
-                for i in S.port.nlargest(3, 'variacionDiaria').tolist():
+                for i in S.port.nlargest(3, 'variacionDiaria').to_dict():
                     c21.caption(f"* {i['simbolo']}:  {i['variacionDiaria']}%")
                 c22.subheader(':red[Top Loosers]')
-                for i in S.port.nsmallest(3, 'variacionDiaria').tolist():
+                for i in S.port.nsmallest(3, 'variacionDiaria').to_dict():
                     c22.caption(f"* {i['simbolo']}:  {i['variacionDiaria']}%")
 
         with t_acc:
