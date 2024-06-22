3d9181d0b4bed4a44354fed858eaae18fd6e9b23
FedericoBohl <federicobohl@uca.edu.ar>
2024-06-22 18:52:36 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -259,8 +259,8 @@ if 'iol' in S:
             c2.plotly_chart(fig,use_container_width=True)
 
         with t_bon:
-            #_=S.iol.get_operaciones()
-            _=f"{S.iol.base_url}/operaciones?filtro.estado=todas&filtro.fechaDesde=2020-01-01&filtro.fechaHasta={datetime.today().strftime('%Y-%m-%d')}&filtro.pais=argentina"
+            _=S.iol.get_operaciones()
+            #_=f"{S.iol.base_url}/operaciones?filtro.estado=todas&filtro.fechaDesde=2020-01-01&filtro.fechaHasta={datetime.today().strftime('%Y-%m-%d')}&filtro.pais=argentina"
             st.write(_)
 else:st.warning('No se ha podido iniciar sesion. Compruebe sus credenciales')
 
