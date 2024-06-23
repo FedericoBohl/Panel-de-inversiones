882ce88f9f9394c8d920df9a11f25172f6d0370e
FedericoBohl <federicobohl@uca.edu.ar>
2024-06-23 16:18:03 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,8 +186,8 @@ def calcular_proffit_bonos(his_op,_now_):
         else:
             profit_acciones.at[profit_acciones.index[i], 'Ganancia%'] = None
     profit_acciones=profit_acciones.dropna().sort_values(by='Ganancia%', ascending=True)
-    #montos=[S.port[S.port['simbolo']==i].to_list()[0][1] for i in profit_acciones.index]
-    #profit_acciones['Monto']=montos
+    montos=[S.port[S.port['simbolo']==1].values.tolist()[0][1] for i in profit_acciones.index]
+    profit_acciones['Monto']=montos
     return profit_acciones[profit_acciones['Cantidad']>0]
 with st.sidebar:
     with st.form('Login',border=False):
