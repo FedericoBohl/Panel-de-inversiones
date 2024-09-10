b628c3bfbf3d456e75840c29a9fd1db6b81813d5
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-09 23:12:15 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -336,8 +336,8 @@ def rendimiento_portfolio(now):
 
     fig=make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(go.Scatter(x=df_val.index,y=df_val['Portfolio'],name='Portfolio',marker_color='goldenrod'),secondary_y=False)
-    fig.add_trace(go.Scatter(x=spy.index,y=spy*100,name='SPY',marker_color='royalblue'),secondary_y=True)
-    fig.add_trace(go.Scatter(x=var_pond.index,y=var_pond['Portfolio']*100,name='Rendimiento Portfolio',marker_color='crimson',fill='tonexty',fillcolor='lightgreen'),secondary_y=True)
+    fig.add_trace(go.Scatter(x=spy.index,y=spy*100,name='SPY',marker_color='royalblue',mode='lines'),secondary_y=True)
+    fig.add_trace(go.Scatter(x=var_pond.index,y=var_pond['Portfolio']*100,name='Rendimiento Portfolio',marker_color='crimson',mode='lines'),secondary_y=True)
     fig.update_layout(hovermode="x unified", margin=dict(l=1, r=1, t=25, b=1),height=450,bargap=0.2,legend=dict(
                                         orientation="h",
                                         yanchor="bottom",
