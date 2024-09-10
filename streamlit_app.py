0887939b6d10188f0206430b6dd8d4055024336e
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-10 18:02:30 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -471,10 +471,10 @@ if 'iol' in S:
             c2.subheader('Analisis por activo')
             asset='VIST'
             fig=go.Figure()
-            fig.add_trace(go.Scatter(x=vars_usd.index,y=vars_usd[asset]*100,name=asset,marker_color='crimson',mode='lines'))
-            fig.add_trace(go.Scatter(x=var_pond.index,y=var_pond['Portfolio']*100,name='Portfolio',marker_color='lavenderblush',mode='lines'))
             fig.add_trace(go.Scatter(x=vars_usd.index,y=vars_usd['SPY']*100,name='SPY',marker_color='darkgreen',mode='lines',line=dict(width=2)))
             fig.add_trace(go.Scatter(x=vars_usd.index,y=vars_usd['SPY']*0,name='None',showlegend=False,marker_color='mediumspringgreen',mode='none',line=dict(dash='dashdot'),fillcolor='mediumspringgreen',fill='tonexty'))
+            fig.add_trace(go.Scatter(x=vars_usd.index,y=vars_usd[asset]*100,name=asset,marker_color='crimson',mode='lines'))
+            fig.add_trace(go.Scatter(x=var_pond.index,y=var_pond['Portfolio']*100,name='Portfolio',marker_color='lavender',mode='lines'))
             fig.add_hline(y=0,line_dash="dot",secondary_y=True,line_color="white",line_width=2)
             fig.update_layout(hovermode="x unified", margin=dict(l=1, r=1, t=25, b=1),height=450,bargap=0.2,legend=dict(
                                                 orientation="h",
